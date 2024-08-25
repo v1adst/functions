93c180699e156b56bf14ee40ba198438e249ef57
v1adst <141177577+v1adst@users.noreply.github.com>
2024-08-25 21:53:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function calcAverage(a, b, c) {
+  average = (a + b + c) / 3;
+  console.log(`Scorul Delfinilor: ${average} puncte.`);
+  return average;
+}
+
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins >= avgKoalas * 2) {
+    console.log(`Delfinii au cîștigat!\n| D - ${avgDolphins} puncte | K - ${avgKoalas} puncte |`);
+  } 
+  else if (avgKoalas >= avgDolphins * 2) {
+    console.log(`Koalas au cîștigat!\nK - ${avgKoalas}puncte | K - ${avgDolphins}puncte`);
+  } 
+  else {
+    console.warn('Nicio echipă nu a cîștigat.');
+  }
+}
+
+console.log('Testul 1');
+avgDolphins = calcAverage(44, 23, 71);
+avgKoalas = calcAverage(65, 54, 49);
+checkWinner(avgDolphins, avgKoalas);
+
+console.log('Testul 2');
+
+avgDolphins = calcAverage(85, 54, 41);
+avgKoalas = calcAverage(23, 34, 27);
+checkWinner(avgDolphins, avgKoalas);
